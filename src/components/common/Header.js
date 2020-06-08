42675d44bb7cf04cc71e536da401fc0982b0ade2
mariusweb <39734577+mariusweb@users.noreply.github.com>
2020-06-08 19:59:51 +0300

    Added some styling

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,27 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import classes from "./Header.module.css";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className={classes.headerStyle}>
-      <div className={classes.navStyle}>
+    <header className={styles.headerStyle}>
+      <div className={styles.headStyle}>
         <div className="logo">
-          <NavLink to="/" exact>
+          <NavLink
+            to="/"
+            exact
+            className={`${styles.navLink} ${styles.logoLink}`}
+          >
             <h1>__1981__</h1>
           </NavLink>
         </div>
         {" | "}
         <nav>
-          <NavLink to="/" exact>
+          <NavLink to="/" exact className={styles.navLink}>
             Home
           </NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/work">Work</NavLink>
-          <NavLink to="/blog">Blog</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/about" className={styles.navLink}>
+            About
+          </NavLink>
+          <NavLink to="/work" className={styles.navLink}>
+            Work
+          </NavLink>
+          <NavLink to="/blog" className={styles.navLink}>
+            Blog
+          </NavLink>
+          <NavLink to="/contact" className={styles.navLink}>
+            Contact
+          </NavLink>
         </nav>
-        <div></div>
+        <form>
+          <input
+            className={styles.place}
+            type="search"
+            name="Welcome Back"
+            id=""
+            placeholder="Welcome Back,"
+          />
+          <input type="submit" value="Client Sign-Out" />
+        </form>
       </div>
     </header>
   );
